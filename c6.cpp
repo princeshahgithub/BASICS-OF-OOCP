18bcfbc2349a8289045b02335c7d4fc7bbf25899
Prince Shah <princeashvinshah@gmail.com>
2024-09-23 22:15:44 +0530

    C++ Basics

---
+++ c6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x;
+    cout << "Value of x:" << x << endl;
+    cout << "Value of y:" << y ;
+    return 0;
+}
---
+++ c7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+//This one includes all the libraries of C++ by which we don't have to write many libraries
+
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x;
+    cout << "Value of x:" << x << endl;
+    cout << "Value of y:" << y ;
+    return 0;
+}
