22f71e4145598d5620b8239b21fbda6bfe3129ac
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-04-30 21:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (90.57%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int res = 0;
+
+        for (int n : nums) {
+            if ((n > 9 && n < 100) || (n > 999 && n < 10000) || n == 100000) {
+                res++;
+            }
+        }
+
+        return res;        
+    }
+};
