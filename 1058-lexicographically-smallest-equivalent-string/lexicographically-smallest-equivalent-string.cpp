4924f00c6eb46facee14d9d713c1377fa7ed2cf5
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-06-05 08:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (96.66%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,32 @@
+class UnionFind { // A simple version for UnionFind class
+    vector<int> root;
+public:
+    UnionFind(int n):root(n) {
+        iota(root.begin(), root.end(), 0);
+    }
+
+    int Find(int x) { //Path compression
+        return (x==root[x]) ? x : root[x] = Find(root[x]);
+    }
+
+    void Union(int x, int y) {// the smaller one as the common root
+        x= Find(x), y= Find(y);
+        if (x == y) return;
+        else if (x<y) root[y]=x;  
+        else root[x]=y;  
+    }
+};
+class Solution {
+public:
+    string smallestEquivalentString(string& s1, string& s2, string& baseStr) {
+        UnionFind G(26);
+        const int n=s1.size(), m=baseStr.size();
+        for(int i=0; i<n; i++)
+            G.Union(s1[i]-'a', s2[i]-'a');
+        
+        string ans(m, ' ');
+        for(int i=0; i<m; i++)
+            ans[i]='a'+G.Find(baseStr[i]-'a');
+        return ans;
+    }
+};
