6aa0bc6cb4d35bcb27d2f9a795b7360dfb853bab
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-07-11 11:55:05 +0530

    Time: 62 ms (95.44%) | Memory: 99.4 MB (46.06%) - LeetSync

---
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    using info=pair<long long, int>;// (ending time, room number)
+    static int mostBooked(int n, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        vector<int> count(n, 0);
+        vector<int> freeR(n);
+        iota(freeR.begin(), freeR.end(), 0);
+        priority_queue<int, vector<int>, greater<int>> freeRoom(freeR.begin(), freeR.end());
+        priority_queue<info, vector<info>, greater<info>> used;
+
+        for (auto& meet: meetings){
+            int room;
+            while (!used.empty() && used.top().first<=meet[0]){
+                room=used.top().second;
+                freeRoom.push(room);
+                used.pop();
+            }
+            long long start, m_time=meet[1]-meet[0];
+            if (freeRoom.empty()){
+                tie(start, room)=used.top();
+                used.pop();
+            }
+            else{
+                room=freeRoom.top();
+                start=meet[0];
+                freeRoom.pop();
+            }
+            count[room]++;
+            used.push({start+m_time, room});
+        }
+        int idx=max_element(count.begin(), count.end())-count.begin();
+
+        return idx;
+    }
+};
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
