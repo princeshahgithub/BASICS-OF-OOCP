6d17f3d25bfd0e640c9bcca1635ed08efc289d3c
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-10-18 23:01:54 +0530

    Time: 131 ms (32.81%) | Memory: 97.5 MB (88.33%) - LeetSync

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        long long prev = LLONG_MIN;
+        for (int i : nums){
+            if (prev < i - k){
+                prev = i - k;
+                res++;
+            } else if (prev < i + k){
+                prev = prev + 1;
+                res++;
+            }
+        }
+        return res;
+    }
+};
