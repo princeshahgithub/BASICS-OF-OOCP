0836045fdb6a38439ba2389b1858bf8d16d4100b
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-10-20 14:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (74.44%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int X = 0;
+
+        for(int i=0; i<operations.size(); i++){
+            if(operations[i] == "++X" || operations[i] == "X++")
+                X++;
+            else
+                X--;
+        }
+
+        return X;
+    }
+};
