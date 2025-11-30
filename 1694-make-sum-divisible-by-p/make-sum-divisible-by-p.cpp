099f96019a12397be971957346ac6a9309738199
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-11-30 23:18:54 +0530

    Time: 84 ms (48.35%) | Memory: 87.8 MB (74.34%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        int rem = accumulate(nums.begin(), nums.end(), 0LL) % p, pre = 0, res = nums.size();
-        if (!rem) return 0;
-        unordered_map<int, int> mp = {{0, -1}};
-        for (int i = 0; i < nums.size(); ++i) {
-            pre = (pre + nums[i]) % p;
-            if (mp.count((pre - rem + p) % p)) 
-                res = min(res, i - mp[(pre - rem + p) % p]);
-            mp[pre] = i;
+        long long total = 0;
+        for (int x : nums) total += x;
+        int need = total % p;
+        if (need == 0) return 0;
+
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        long long prefix = 0;
+        int res = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix = (prefix + nums[i]) % p;
+            int target = (prefix - need + p) % p;
+            if (mp.count(target)) res = min(res, i - mp[target]);
+            mp[prefix] = i;
         }
+
         return res == nums.size() ? -1 : res;
     }
 };
-auto io_opt = [] { ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
