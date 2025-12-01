c141d2d0a91ea744841dd86c24e1ec91d6e4f570
Prince Shah <137589854+princeshahgithub@users.noreply.github.com>
2025-12-01 23:35:22 +0530

    Time: 23 ms (63.69%) | Memory: 59.7 MB (58.92%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        long long total = accumulate(arr.begin(), arr.end(), 0LL);
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            if (arr[i] <= total / n) break;
+            total -= arr[i];
+            n--;
+        }
+
+        return total / n;
+    }
+};
